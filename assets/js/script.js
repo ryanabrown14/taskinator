6dc225bdeca8255c9dda6f9b59588ae94738c055
ry brown <ryan.alex.brown@gmail.com>
2020-08-19 17:00:11 -0700

    inner html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,22 @@ var tasksToDoEL = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
-    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+
     var listItemEL =document.createElement("li");
     listItemEL.className = "task-item";
-    listItemEL.textContent = "this is a new task.";
+    //create div to hold task info and add to list item
+    var taskInfoEL = document.createElement("div");
+    //give it a class name
+    taskInfoEL.className = "task-info";
+    //add html content to div
+    taskInfoEL.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEL.appendChild(taskInfoEL);
+    
+    // add entire list item to list
     tasksToDoEL.appendChild(listItemEL);
 
 }
